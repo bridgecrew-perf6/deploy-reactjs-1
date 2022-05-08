b25fb0a392ce4dc41ce4a603bfa77e74c5893230
mvn-xinhnguyen2-dn <xinh.nguyen2@monstar-lab.com>
2022-05-08 12:22:44 +0700

    update banner

--- src/pages/Features/Home/Banner.js
+++ src/pages/Features/Home/Banner.js
@@ -4,9 +4,9 @@ export default function Banner() {
   return (
     <div className="banner-image">
       <div className="banner-text">
-        <h1>I am John Doe</h1>
-        <p>And I'm a Photographer</p>
-        <button>Hire me</button>
+        <h1>Banner Hero</h1>
+        <p>And This is my website</p>
+        <button>See me</button>
       </div>
     </div>
   );
